2cf2e45f39dd8f72c930dd0b57912380b511965d
NuiN99 <endyverse@gmail.com>
2024-01-07 20:56:06 -0800

    QOL

--- Runtime/ScriptableHarmony/Internal/Helpers/InternalEnums.cs
+++ Runtime/ScriptableHarmony/Internal/Helpers/InternalEnums.cs
@@ -2,7 +2,7 @@ namespace NuiN.ScriptableHarmony.Internal.Helpers
 {
     internal enum Access { Getter, Setter }
     internal enum SOType { Variable, ListVariable, DictionaryVariable, RuntimeSet, RuntimeSingle }
-    internal enum ResetOn { ExitPlayMode, SceneLoad }
+    internal enum ResetOn { SceneLoad, ExitPlayMode }
     internal enum LifetimeType { OnEnableOnDisable, OnAwakeOnDestroy, OnlyRemoveOnDestroy, OnlyRemoveOnDestroyAndDisable }
 }
 
