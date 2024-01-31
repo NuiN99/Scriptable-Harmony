c3a7573bbb1840d27c84ed8c6f6054c5bc445873
NuiN99 <endyverse@gmail.com>
2024-01-30 23:05:38 -0800

    Random timer options

--- Runtime/NExtensions/Classes/Timer.cs
+++ Runtime/NExtensions/Classes/Timer.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace NuiN.NExtensions
 {
@@ -8,11 +9,26 @@ namespace NuiN.NExtensions
     {
         float _timeStart;
     
-        [SerializeField, Tooltip("How long before the timer completes")] 
+        [SerializeField, Tooltip("Automatically set random duration upon completion")]
+        bool randomDuration;
+
+        [SerializeField, ShowIf(nameof(randomDuration), true), Tooltip("Min possible time before the timer completes")]
+        float minDuration;
+        
+        [SerializeField, ShowIf(nameof(randomDuration), true), Tooltip("Max possible time before the timer completes")]
+        float maxDuration;
+
+        [SerializeField, ShowIf(nameof(randomDuration), false), Tooltip("How long before the timer completes")] 
         float duration;
         
         [SerializeField, Tooltip("Makes the timer return completed on the first call")] 
         bool startCompleted = true;
+
+        public Timer InitRandom()
+        {
+            duration = Random.Range(minDuration, maxDuration);
+            return this;
+        }
 
         public Timer SetDuration(float duration)
         {
@@ -32,8 +48,10 @@ namespace NuiN.NExtensions
             }
 
             if (time - _timeStart <= duration) return false;
-        
+                    
             _timeStart = time;
+
+            if(randomDuration) InitRandom();
             return true;
         }
     }
