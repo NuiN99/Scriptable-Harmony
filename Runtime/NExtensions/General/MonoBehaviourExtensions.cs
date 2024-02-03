57d8f586d95ff6d2aa13a0d3e7f3da621c159c78
NuiN99 <endyverse@gmail.com>
2024-02-02 18:52:43 -0800

    Update MonoBehaviourExtensions.cs

--- Runtime/NExtensions/General/MonoBehaviourExtensions.cs
+++ Runtime/NExtensions/General/MonoBehaviourExtensions.cs
@@ -2,70 +2,73 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public static class MonoBehaviourExtensions
+namespace NuiN.NExtensions
 {
-    public static void TryStopCoroutine(this MonoBehaviour behaviour, Coroutine coroutine)
+    public static class MonoBehaviourExtensions
     {
-        if(coroutine != null) behaviour.StopCoroutine(coroutine);
-    }
-
-    public static void ReplaceCoroutine(this MonoBehaviour behaviour, Coroutine coroutine, IEnumerator enumerator)
-    {
-        behaviour.TryStopCoroutine(coroutine);
-        coroutine = behaviour.StartCoroutine(enumerator);
-    }
-
-    // ReSharper disable Unity.PerformanceAnalysis
-    public static Coroutine DoAfter(this MonoBehaviour behaviour, float seconds, Action onComplete)
-    {
-        return behaviour.StartCoroutine(DoAfterCoroutine(seconds, onComplete));
-    }
-    
-    // ReSharper disable Unity.PerformanceAnalysis
-    public static Coroutine DoWhen(this MonoBehaviour behaviour, Func<bool> condition, Action onComplete)
-    {
-        return behaviour.StartCoroutine(DoWhenCoroutine(condition, onComplete));
-    }
-    
-    // ReSharper disable Unity.PerformanceAnalysis
-    public static Coroutine DoFor(this MonoBehaviour behaviour, float seconds, Action onUpdate, Action onComplete = null, Func<bool> stopIf = null, Action onStop = null)
-    {
-        return behaviour.StartCoroutine(DoForCoroutine(seconds, onUpdate, onComplete, stopIf, onStop));
-    }
-
-    // ReSharper disable Unity.PerformanceAnalysis
-    static IEnumerator DoAfterCoroutine(float seconds, Action onComplete)
-    {
-        yield return new WaitForSeconds(seconds);
-        onComplete?.Invoke();
-    }
-    
-    // ReSharper disable Unity.PerformanceAnalysis
-    static IEnumerator DoWhenCoroutine(Func<bool> condition, Action onComplete)
-    {
-        yield return new WaitUntil(condition);
-        onComplete?.Invoke();
-    }
-    
-    // ReSharper disable Unity.PerformanceAnalysis
-    static IEnumerator DoForCoroutine(float seconds, Action onUpdate, Action onComplete, Func<bool> stopIf, Action onStop)
-    {
-        bool nullCondition = stopIf == null;
-        
-        float time = 0f;
-        while (time < seconds)
+        public static void StopCoroutineSafe(this MonoBehaviour behaviour, Coroutine coroutine)
         {
-            if (!nullCondition && stopIf.Invoke())
-            {
-                onStop?.Invoke();
-                yield break;
-            }
-            
-            time += Time.deltaTime;
-            onUpdate?.Invoke();
-            yield return null;
+            if(coroutine != null) behaviour.StopCoroutine(coroutine);
+        }
+
+        public static Coroutine ReplaceCoroutine(this MonoBehaviour behaviour, Coroutine coroutine, IEnumerator enumerator)
+        {
+            behaviour.StopCoroutineSafe(coroutine);
+            return behaviour.StartCoroutine(enumerator);
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        public static Coroutine DoAfter(this MonoBehaviour behaviour, float seconds, Action onComplete)
+        {
+            return behaviour.StartCoroutine(DoAfterCoroutine(seconds, onComplete));
         }
         
-        onComplete?.Invoke();
+        // ReSharper disable Unity.PerformanceAnalysis
+        public static Coroutine DoWhen(this MonoBehaviour behaviour, Func<bool> condition, Action onComplete)
+        {
+            return behaviour.StartCoroutine(DoWhenCoroutine(condition, onComplete));
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        public static Coroutine DoFor(this MonoBehaviour behaviour, float seconds, Action onUpdate, Action onComplete = null, Func<bool> stopIf = null, Action onStop = null)
+        {
+            return behaviour.StartCoroutine(DoForCoroutine(seconds, onUpdate, onComplete, stopIf, onStop));
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        static IEnumerator DoAfterCoroutine(float seconds, Action onComplete)
+        {
+            yield return new WaitForSeconds(seconds);
+            onComplete?.Invoke();
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        static IEnumerator DoWhenCoroutine(Func<bool> condition, Action onComplete)
+        {
+            yield return new WaitUntil(condition);
+            onComplete?.Invoke();
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        static IEnumerator DoForCoroutine(float seconds, Action onUpdate, Action onComplete, Func<bool> stopIf, Action onStop)
+        {
+            bool nullCondition = stopIf == null;
+            
+            float time = 0f;
+            while (time < seconds)
+            {
+                if (!nullCondition && stopIf.Invoke())
+                {
+                    onStop?.Invoke();
+                    yield break;
+                }
+                
+                time += Time.deltaTime;
+                onUpdate?.Invoke();
+                yield return null;
+            }
+            
+            onComplete?.Invoke();
+        }
     }
 }
