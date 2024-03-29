fcb85359b60021c4511b95e3ed7e317f1a32ad7d
NuiN99 <endyverse@gmail.com>
2024-03-28 19:08:11 -0700

    CollisionProxy class

---
+++ Runtime/NExtensions/Classes/CollisionProxy.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+namespace NuiN.NExtensions
+{
+    public class CollisionProxy : MonoBehaviour
+    {
+        public event Action<Collision> CollisionEnter;
+        public event Action<Collision> CollisionExit;
+        public event Action<Collision> CollisionStay;
+        
+        public event Action<Collider> TriggerEnter;
+        public event Action<Collider> TriggerExit;
+        public event Action<Collider> TriggerStay;
+        
+        void OnCollisionEnter(Collision other) => CollisionEnter?.Invoke(other);
+        void OnCollisionExit(Collision other) => CollisionExit?.Invoke(other);
+        void OnCollisionStay(Collision other) => CollisionStay?.Invoke(other);
+        void OnTriggerEnter(Collider other) => TriggerEnter?.Invoke(other);
+        void OnTriggerExit(Collider other) => TriggerExit?.Invoke(other);
+        void OnTriggerStay(Collider other) => TriggerStay?.Invoke(other);
+    }
+}
+
