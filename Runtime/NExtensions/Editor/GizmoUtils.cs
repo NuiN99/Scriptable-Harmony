c01d89b1db58a6e31092d0c7d070f7e8b116cf9e
NuiN99 <endyverse@gmail.com>
2024-08-07 18:36:57 -0700

    feat: draw label gizmo

--- Runtime/NExtensions/Editor/GizmoUtils.cs
+++ Runtime/NExtensions/Editor/GizmoUtils.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 namespace NuiN.NExtensions.Editor
 {
@@ -24,6 +25,51 @@ namespace NuiN.NExtensions.Editor
             }
             
             Debug.DrawLine(points[^1] + position, points[0] + position, color);
+        }
+
+        public static void DrawString(string text, int fontSize, Vector3 worldPos, Color? colour = null, Vector2 offset = new()) 
+        {
+    #if UNITY_EDITOR
+            var view = UnityEditor.SceneView.currentDrawingSceneView;
+            Camera cam = view == null ? Camera.main : view.camera;
+            if (cam == null) return;
+            
+            UnityEditor.Handles.BeginGUI();
+
+            Vector3 screenPos = cam.WorldToScreenPoint(worldPos);
+
+            if (screenPos.y < 0 || screenPos.y > Screen.height || screenPos.x < 0 || screenPos.x > Screen.width || screenPos.z < 0) {
+                UnityEditor.Handles.EndGUI();
+                return;
+            }
+
+            float distFromCam = Vector3.Distance(worldPos, cam.transform.position);
+            int roundedDistFromCam = Mathf.RoundToInt(distFromCam);
+            
+            // too far to be visible, so don't draw
+            if (roundedDistFromCam <= 0)
+            {
+                return;
+            }
+
+            fontSize /= roundedDistFromCam;
+
+            GUIStyle style = new GUIStyle
+            {
+                fontSize = fontSize,
+                normal = { textColor = colour ?? Color.white }
+            };
+            
+            float width = style.CalcSize(new GUIContent(text)).x;
+            float widthOffset = width / 2;
+            float heightOffset = -(style.CalcHeight(new GUIContent(text), width) / 2);
+            Vector2 sizeOffset = new Vector2(widthOffset, heightOffset);
+            offset -= sizeOffset;
+            
+            UnityEditor.Handles.Label(cam ? cam.ScreenToWorldPoint(cam.WorldToScreenPoint(worldPos) + (Vector3)offset) : worldPos, text, style);
+
+            UnityEditor.Handles.EndGUI();
+    #endif
         }
     }
 }
