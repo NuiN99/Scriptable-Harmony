ac23d4e6fdd6bde0eaf5e77c94a239b705bdddb6
NuiN99 <endyverse@gmail.com>
2024-11-05 00:16:02 -0800

    Update ReadOnlyPlayModeAttribute.cs

--- Runtime/NExtensions/Attributes/ReadOnlyPlayModeAttribute.cs
+++ Runtime/NExtensions/Attributes/ReadOnlyPlayModeAttribute.cs
@@ -30,4 +30,32 @@ namespace NuiN.NExtensions
         }
     }
 #endif
+
+    public class ReadOnlyEditorModeAttribute : PropertyAttribute { }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnlyEditorModeAttribute))]
+    internal class ReadOnlyEditorModeDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return !Application.isPlaying ? EditorGUI.GetPropertyHeight(property, label, true) : 0;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            int indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            if (!Application.isPlaying)
+            {
+                GUI.enabled = false;
+                EditorGUI.PropertyField(position, property, label, true);
+                GUI.enabled = true;
+            }
+
+            EditorGUI.indentLevel = indent;
+        }
+    }
+#endif
 }
