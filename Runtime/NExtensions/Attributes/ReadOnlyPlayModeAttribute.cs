7673c52f066bae417c928507bdb71284040047d6
NuiN99 <endyverse@gmail.com>
2024-11-07 15:51:16 -0800

    feat: hideinplaymode attr

--- Runtime/NExtensions/Attributes/ReadOnlyPlayModeAttribute.cs
+++ Runtime/NExtensions/Attributes/ReadOnlyPlayModeAttribute.cs
@@ -30,4 +30,30 @@ namespace NuiN.NExtensions
         }
     }
 #endif
+
+    public class HideInPlayModeAttribute : PropertyAttribute { }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(HideInPlayModeAttribute))]
+    internal class HideInPlayModeDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return Application.isPlaying ? EditorGUI.GetPropertyHeight(property, label, true) : 0;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            int indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            if (!Application.isPlaying)
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+            }
+
+            EditorGUI.indentLevel = indent;
+        }
+    }
+#endif
 }
