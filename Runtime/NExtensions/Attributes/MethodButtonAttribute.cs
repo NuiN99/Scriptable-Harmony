629a6057b73dd1b8eec8395b43b60ef533473f9b
NuiN99 <endyverse@gmail.com>
2024-11-08 18:03:56 -0800

    fix: method button attribute

--- Runtime/NExtensions/Attributes/MethodButtonAttribute.cs
+++ Runtime/NExtensions/Attributes/MethodButtonAttribute.cs
@@ -8,11 +8,9 @@ namespace NuiN.NExtensions
     {
         public readonly string label;
         public readonly bool onlyShowInPlayMode;
-        public readonly object[] parameters;
         public MethodButtonAttribute(string label, bool onlyShowInPlayMode = false, object[] parameters = null)
         {
             this.label = label;
-            this.parameters = parameters;
             this.onlyShowInPlayMode = onlyShowInPlayMode;
         }
     }
--- Runtime/NExtensions/Editor/AttributesEditor.cs
+++ Runtime/NExtensions/Editor/AttributesEditor.cs
@@ -1,56 +1,129 @@
 #if UNITY_EDITOR
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace NuiN.NExtensions
 {
-	[CustomEditor( typeof( MonoBehaviour ), true ), CanEditMultipleObjects]
-	public class AttributesEditor : UnityEditor.Editor
-	{
-		void OnEnable() => RuntimeHelper.SubOnUpdate(Repaint);
-		void OnDisable() => RuntimeHelper.SubOnUpdate(Repaint);
+	[CustomEditor(typeof(Object), true), CanEditMultipleObjects]
+    public class AttributesEditor : UnityEditor.Editor
+    {
+        void OnEnable() => RuntimeHelper.SubOnUpdate(Repaint);
+        void OnDisable() => RuntimeHelper.UnSubOnUpdate(Repaint);
 
-		public override void OnInspectorGUI()
-		{
-			MonoBehaviour script = (MonoBehaviour)target;
-        
-			// draw script field manually to make sure it's at the top
-			GUI.enabled = false;
-			EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour(script), typeof(MonoScript), false);
-			GUI.enabled = true;
+        public override void OnInspectorGUI()
+        {
+            MonoBehaviour monoTarget = target as MonoBehaviour;
+            ScriptableObject soTarget = target as ScriptableObject;
 
-			// draw method buttons
-			MethodInfo[] methods = script.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-				.Where(method => method.GetCustomAttributes(typeof(MethodButtonAttribute), true).Length > 0)
-				.ToArray();
-			foreach (var method in methods)
-			{
-				MethodButtonAttribute attribute = (MethodButtonAttribute)method.GetCustomAttributes(typeof(MethodButtonAttribute), true)[0];
-				string buttonLabel = attribute == null ? method.Name : attribute.label;
+            if (monoTarget == null && soTarget == null)
+            {
+                DrawDefaultInspector();
+                return;
+            }
 
-				if (attribute == null || (attribute.onlyShowInPlayMode && !Application.isPlaying)) continue;
-				if (GUILayout.Button(buttonLabel)) method.Invoke(script, attribute?.parameters);
-			}
+            GUI.enabled = false;
+            const string scriptLabel = "Script";
+            if (target == monoTarget)
+            {
+                EditorGUILayout.ObjectField(scriptLabel, MonoScript.FromMonoBehaviour(monoTarget), typeof(MonoScript), false);
+            }
+            else if (target == soTarget)
+            {
+                EditorGUILayout.ObjectField(scriptLabel, MonoScript.FromScriptableObject(soTarget), typeof(MonoScript), false);
+            }
+            GUI.enabled = true;
 
-			// draw showininspector fields
-			if (Application.isPlaying)
-			{
-				this.DrawShowInInspector();
-			}
-			
-			// Draw the rest without script field
-			SerializedProperty property = serializedObject.GetIterator();
-			property.NextVisible(true);
-			while (property.NextVisible(false))
-			{
-				EditorGUILayout.PropertyField(property, true);
-			}
-			serializedObject.ApplyModifiedProperties();
-		}
-	}
+            DrawMethodButtons();
+
+            if (Application.isPlaying)
+            {
+                this.DrawShowInInspector();
+            }
+
+            serializedObject.Update();
+
+            SerializedProperty property = serializedObject.GetIterator();
+            property.NextVisible(true);
+            while (property.NextVisible(false))
+            {
+                EditorGUILayout.PropertyField(property, true);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        void DrawMethodButtons()
+        {
+            var processedMethods = new HashSet<string>();
+            Type currentType = target.GetType();
+            while (currentType != null && currentType != typeof(object))
+            {
+                MethodInfo[] methods = currentType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                    .Where(method => method.GetCustomAttributes(typeof(MethodButtonAttribute), true).Length > 0)
+                    .ToArray();
+
+                foreach (var method in methods)
+                {
+                    string methodSignature = GetMethodSignature(method);
+
+                    if (processedMethods.Contains(methodSignature))
+                        continue;
+
+                    object[] attributes = method.GetCustomAttributes(typeof(MethodButtonAttribute), true);
+                    foreach (var attr in attributes)
+                    {
+                        MethodButtonAttribute attribute = (MethodButtonAttribute)attr;
+                        string buttonLabel = attribute == null ? method.Name : attribute.label;
+
+                        if (attribute == null || (attribute.onlyShowInPlayMode && !Application.isPlaying)) continue;
+
+                        ParameterInfo[] methodParams = method.GetParameters();
+                        object[] defaultParameters = new object[methodParams.Length];
+                        for (int i = 0; i < methodParams.Length; i++)
+                        {
+                            Type paramType = methodParams[i].ParameterType;
+
+                            if (!paramType.IsValueType) continue;
+                            
+                            if (methodParams[i].DefaultValue != null)
+                            {
+                                defaultParameters[i] = methodParams[i].DefaultValue;
+                            }
+                            else
+                            {
+                                defaultParameters[i] = Activator.CreateInstance(paramType);
+                            }
+                        }
+                        
+                        if (GUILayout.Button(buttonLabel))
+                            method.Invoke(target, defaultParameters);
+
+                        processedMethods.Add(methodSignature);
+                    }
+                }
+
+                currentType = currentType.BaseType;
+            }
+
+            return;
+            
+            string GetMethodSignature(MethodInfo method)
+            {
+                ParameterInfo[] parameterInfos = method.GetParameters();
+                var parameterSignatures = parameterInfos
+                    .Select(p => p.ParameterType.FullName + " " + p.Name) // You can choose to omit the name if not needed
+                    .ToArray();
+
+                return method.Name + "(" + string.Join(", ", parameterSignatures) + ")";
+            }
+        }
+    }
 }
 
 #endif
