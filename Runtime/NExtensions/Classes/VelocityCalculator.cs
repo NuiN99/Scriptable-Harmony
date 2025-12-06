27e82b8242c41aee5c7581d20823cf3cff6b971a
NuiN99 <61207572+NuiN99@users.noreply.github.com>
2025-12-06 15:22:19 -0800

    Update VelocityCalculator.cs

--- Runtime/NExtensions/Classes/VelocityCalculator.cs
+++ Runtime/NExtensions/Classes/VelocityCalculator.cs
@@ -5,10 +5,23 @@ namespace NuiN.NExtensions
     public class VelocityCalculator : MonoBehaviour
     {
         [ShowInInspector] public Vector3 Velocity { get; private set; }
+
+        [SerializeField] bool lateUpdate;
+
         Vector3 _position;
         Vector3 _previousPosition;
 
         void Update()
+        {
+            if (!lateUpdate) Calculate();
+        }
+
+        void LateUpdate()
+        {
+            if (lateUpdate) Calculate();
+        }
+
+        void Calculate()
         {
             _previousPosition = _position;
             _position = transform.position;
