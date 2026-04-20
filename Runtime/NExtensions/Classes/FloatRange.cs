d21f464dd465963f023e51a859f8023ee65379f3
NuiN <endyverse@gmail.com>
2026-04-20 15:11:47 -0700

    Update FloatRange.cs

--- Runtime/NExtensions/Classes/FloatRange.cs
+++ Runtime/NExtensions/Classes/FloatRange.cs
@@ -11,8 +11,25 @@ namespace NuiN.NExtensions
 
         public float Lerp(float lerp) => Mathf.Lerp(Min, Max, lerp);
         public float Random() => UnityEngine.Random.Range(Min, Max);
+        public float Clamp(float value) => Mathf.Clamp(value, Min, Max);
 
         public FloatRange(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+
+    [Serializable]
+    public class IntRange
+    {
+        [field: SerializeField] public int Min { get; private set; }
+        [field: SerializeField] public int Max { get; private set; }
+
+        public int Random() => UnityEngine.Random.Range(Min, Max + 1);
+        public int Clamp(int value) => Math.Clamp(value, Min, Max);
+
+        public IntRange(int min, int max)
         {
             Min = min;
             Max = max;
